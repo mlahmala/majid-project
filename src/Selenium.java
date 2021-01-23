fcee9199ada7fc8d6e722aefce81b6389eecc9df
majid <majid.lahmala@gmail.com>
2021-01-23 18:52:52 -0500

    commit1

---
+++ src/Selenium.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Selenium {
+    public static void main(String[] args){
+        String[][] teams = {
+                {"Ylduz", "Mark", "Akylbek", "Daniel"},
+                {"Ariunna", "Frank", "Nurgazy", "Anna"},
+                {"Selvin", "Meerim", "Gulzhana", "Rocio"},
+                {"Job", "Evelyn", "Edil", "Aizhamal"},
+                {"Vlad"}
+        };
+        System.out.println(teams[1][1]);
+        System.out.println(Arrays.deepToString(teams));
+        for (int i = 0; i < teams.length; i++) {
+            for (int j = 0; j < teams[i].length; j++) {
+                teams[i][j] = "John";
+            }
+        }
+        System.out.println(Arrays.deepToString(teams));
+    }
+}
+
+
+
+
