4aaecb8a8010c3f99a0edd7c33b3a082a49b0e06
majid <majid.lahmala@gmail.com>
2021-01-23 19:13:49 -0500

    second commit

---
+++ src/Selenum.java
@@ -0,0 +1,18 @@
+
+
+public class Selenum {
+
+    public static int sum(int num, int num2) {
+        return num + num2;
+    }
+    public static boolean isEven(int num) {
+        if (num % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static boolean isOdd(int num) {
+        return num % 2 != 0;
+    }
+}
